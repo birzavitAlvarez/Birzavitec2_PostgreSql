2ad5510b2acd29978fd86fe87bf6548b23e7f548
Birzavit Alvarez <blaziken.bap@hotmail.com>
2023-07-03 13:55:40 -0500

    equisde

--- src/main/java/com/birzavit/Birzavitec2/controller/IdatController.java
+++ src/main/java/com/birzavit/Birzavitec2/controller/IdatController.java
@@ -1,32 +1,27 @@
 package com.birzavit.Birzavitec2.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.lang.String;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.birzavit.Birzavitec2.entity.Idat;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@RequestMapping()
+@RequestMapping(path="/")
 public class IdatController {
-    // private static final String template = "ALUMNO, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping()
-	public Idat greeting(@RequestParam(value = "name", defaultValue = "A20200702 - BIRZAVIT ALVAREZ PINTO") String name) {
-		return new Idat(counter.incrementAndGet(), String.format( name));
-	}
+    @GetMapping(path="/")
+    public String home(){
+        return "A20200702 - Birzavit Alvarez";
+    }
 
-	@GetMapping("/idat/codigo")
-	public Idat greeting2(@RequestParam(value = "name", defaultValue = "A20200702") String name) {
-		return new Idat(counter.incrementAndGet(), String.format( name));
-	}
+    @GetMapping(path="/idat/codigo")
+    public String cod(){
+        return "A20200702";
+    }
 
-	@GetMapping("/idat/nombre-completo")
-	public Idat greeting3(@RequestParam(value = "name", defaultValue = "BIRZAVIT ALVAREZ PINTO") String name) {
-		return new Idat(counter.incrementAndGet(), String.format( name));
-	}
-
+    @GetMapping(path="/idat/nombre-completo")
+    public String nombrecompleto(){
+        return "Birzavit Alvarez Pinto";
+    }
+    
 }
--- src/main/java/com/birzavit/Birzavitec2/entity/Idat.java
+++
@@ -1,5 +0,0 @@
-package com.birzavit.Birzavitec2.entity;
-
-public record Idat (Long id, String content) {
-    
-}
