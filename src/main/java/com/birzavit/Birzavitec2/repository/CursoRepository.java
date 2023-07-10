08722ddbd5eb7ea2d75fa4dedd7219486d56a3cb
Birzavit Alvarez <blaziken.bap@hotmail.com>
2023-07-10 13:04:18 -0500

    sem8

--- src/main/java/com/birzavit/Birzavitec2/repository/CursoRepository.java
+++ src/main/java/com/birzavit/Birzavitec2/repository/CursoRepository.java
@@ -1,10 +1,17 @@
 package com.birzavit.Birzavitec2.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.birzavit.Birzavitec2.entity.Curso;
 
-@Repository
-public interface CursoRepository extends JpaRepository <Curso, Integer>{
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+
+
+@RepositoryRestResource(collectionResourceRel = "cursos", path = "cursos")
+public interface CursoRepository extends PagingAndSortingRepository<Curso, Integer>,CrudRepository<Curso, Integer> {
     
+    List<Curso> findByNombre(@Param("nombre") String nombre);
+
 }
