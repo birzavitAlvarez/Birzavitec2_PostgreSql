a2ea7e3200f47a70bb0798f99a13de9276268184
Birzavit Alvarez <blaziken.bap@hotmail.com>
2023-07-10 21:45:53 -0500

    tonterías :3

--- src/main/java/com/birzavit/Birzavitec2/repository/CarreraRepository.java
+++ src/main/java/com/birzavit/Birzavitec2/repository/CarreraRepository.java
@@ -1,16 +1,14 @@
 package com.birzavit.Birzavitec2.repository;
+import com.birzavit.Birzavitec2.entity.Carrera;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import com.birzavit.Birzavitec2.entity.Carrera;
 import java.util.List;
 
 
 @RepositoryRestResource(collectionResourceRel = "carreras", path = "carreras")
 public interface CarreraRepository extends PagingAndSortingRepository<Carrera, Integer>,CrudRepository<Carrera, Integer> {
-    
     List<Carrera> findByNombre(@Param("nombre") String nombre);
-
 }
